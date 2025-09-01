c645929da99424ef753a68fd4b48eb11423de23c
cunba <ire.cunba@gmail.com>
2025-09-01 17:52:11 +0200

    fix

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -68,5 +68,76 @@ namespace Models
                 Console.WriteLine("Something went wrong");
             }
         }
+
+        public void Show()
+        {
+            // Create a table
+            var table = new Table
+            {
+                Title = new TableTitle($"Appointment {Id} information")
+            };
+
+            table.AddColumn("Attributtes");
+            table.AddColumn("Information");
+
+            // Add rows
+            table.AddRow("Date", Date.ToString());
+            table.AddRow("Patient", Patient.Name);
+            table.AddRow("Doctor", Doctor.Name);
+            table.AddRow("Address", Doctor.Address);
+            table.AddRow("Specialty", Doctor.Specialty.Name);
+            table.AddRow("Observations", Observations);
+            table.AddRow("Approved", Approved.ToString());
+
+            table.Centered();
+            table.Caption("Press e to exit");
+            table.Columns[0].Padding(2, 4);
+
+            // Render the table to the console
+            AnsiConsole.Write(table);
+        }
+
+        public void ShowList()
+        {
+            var grid = new Grid();
+            grid.AddColumn();
+            grid.AddColumn();
+
+            grid.AddRow(new Text[]{
+                new Text("Date:").RightJustified(),
+                new Text(Date.ToString())
+            });
+            grid.AddRow(new Text[]{
+                new Text("Patient:").RightJustified(),
+                new Text(Patient.Name)
+            });
+            grid.AddRow(new Text[]{
+                new Text("Doctor:").RightJustified(),
+                new Text(Doctor.Name)
+            });
+            grid.AddRow(new Text[]{
+                new Text("Address:").RightJustified(),
+                new Text(Doctor.Address)
+            });
+            grid.AddRow(new Text[]{
+                new Text("Specialty:").RightJustified(),
+                new Text(Doctor.Specialty.Name)
+            });
+            grid.AddRow(new Text[]{
+                new Text("Observations:").RightJustified(),
+                new Text(Observations)
+            });
+            grid.AddRow(new Text[]{
+                new Text("Approved:").RightJustified(),
+                new Text(Approved.ToString())
+            });
+
+            var panel = new Panel(grid)
+            .Border(BoxBorder.Rounded)
+            .Header($"ID {Id.ToString()}", Justify.Center);
+            panel.Width = 65;
+
+            AnsiConsole.Write(panel);
+        }
     }
 }
